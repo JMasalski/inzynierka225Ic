848b1c9568bdeac81518002a5bd457474fd04fcc
JMasalski <masalski.kuba123@gmail.com>
2025-12-12 18:31:56 +0100

    (feat) saving task to db disply task on homepage display indiviudal task

---
+++ frontend/app/(protected)/home/components/TaskCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface TaskCardProps {
+    taskId: string;
+    title: string;
+    createdBy: string;
+
+}
+const TaskCard = () => {
+    return (
+        <div>TaskCard</div>
+    )
+}
+export default TaskCard
